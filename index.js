f77e10b6b86c74c3b30730b51b164f9e8ed5cf81
mattr21 <matt.rothstein21@gmail.com>
2019-03-21 15:40:02 -0700

    made it so we can deploy on Heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('\n*** API running on port 4k ***\n')
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log(`\n*** API running on port ${port} ***\n`)
 });
